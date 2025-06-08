3eb8548447f2c2396e1cb501035608052b444522
JackAdamsJenkins <clensx@gmail.com>
2025-06-08 11:49:46 +0200

    Initial commit

---
+++ inertia/pages/about.tsx
@@ -0,0 +1,16 @@
+import { Head } from '@inertiajs/react'
+
+export default function About() {
+  return (
+    <>
+      <Head title="About page" />
+
+      <div>
+
+        <p className="mt-4 text-sm text-sand-11 group-hover:text-sand-12 transition ease-in-out duration-100">
+          Page about affichage
+        </p>
+      </div>
+    </>
+  )
+}
